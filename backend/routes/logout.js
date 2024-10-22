5970ec5195644be0e051c57148802a1eb662849e
Kaillr <kaillerplayz@gmail.com>
2024-10-22 13:44:36 +0200

    feat: Add logout confirmation

--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    res.render("logout", {
+        title: "Logout",
+    });
+});
+
+router.post("/", (req, res) => {
     req.session.destroy(err => {
         if (err) {
             console.error(err);
